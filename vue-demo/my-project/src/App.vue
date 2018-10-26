65f16a7be207ce5fb5359589b90c63d799d847e0
15982116884 <candy_2446376952@163.com>
2018-10-26 16:43:29 +0800

    编写头部组件

--- vue-demo/my-project/src/App.vue
+++ vue-demo/my-project/src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
-      <h1>hello world</h1>
+    <MHeader></MHeader>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script type="text/ecmascript-6">
+  import MHeader from "./components/m-header/m-header"
+
+  export default {
+    components: {
+      MHeader
+    }
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
